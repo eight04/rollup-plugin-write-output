f59a23ad0a7084ce163b9ed3b5703f77ec89bf2f
eight04 <eight04@gmail.com>
2021-02-12 21:51:18 +0800

    Add: an import resolver to extract execution order

--- index.js
+++ index.js
@@ -7,6 +7,31 @@ function applyMatchRef(s, arr) {
   return s.replace(/\$(\d+)/g, (m, n) => arr[Number(n)] || m);
 }
 
+function createImportResolver(bundle) {
+  const cache = new Map;
+  return key => {
+    if (!cache.has(key)) {
+      cache.set(key, [...search(key, new Set)]);
+    }
+    return cache.get(key);
+  };
+  
+  function* search(key, colored) {
+    if (cache.has(key)) {
+      yield* cache.get(key);
+      return;
+    }
+    if (colored.has(key)) {
+      return;
+    }
+    colored.add(key);
+    for (const subKey of bundle[key].imports) {
+      yield* search(subKey, colored);
+    }
+    yield key;
+  }
+}
+
 function createPlugin(targets) {
   return {
     name,
@@ -14,6 +39,7 @@ function createPlugin(targets) {
   };
 
   async function writeBundle(options, bundle) {
+    const importResolver = createImportResolver(bundle);
     for (const key in bundle) {
       let match, target;
       for (target of targets) {
@@ -27,10 +53,7 @@ function createPlugin(targets) {
       const prefix = options.dir;
       const absTarget = path.resolve(applyMatchRef(target.target, match));
       
-      const scripts = [
-        ...bundle[key].imports,
-        key
-      ]
+      const scripts = [...importResolver(key)]
         .map(p => path.resolve(prefix, p))
         .map(p => path.relative(path.dirname(absTarget), p))
         .map(p => p.replace(/\\/g, "/"));
